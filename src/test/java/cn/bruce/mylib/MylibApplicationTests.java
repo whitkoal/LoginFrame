2e8755a23fb625d81dc81aa4277dded9b5a8c8f5
whitkoal <46294890+whitkoal@users.noreply.github.com>
2019-12-31 17:17:08 +0800

    3

---
+++ src/test/java/cn/bruce/mylib/MylibApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.bruce.mylib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
