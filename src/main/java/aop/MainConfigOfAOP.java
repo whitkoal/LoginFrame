6a8c14f082f43646f45bbd614257ca93b669aaf5
whitkoal <46294890+whitkoal@users.noreply.github.com>
2020-02-26 09:10:10 +0800

    2.26

---
+++ src/main/java/aop/MainConfigOfAOP.java
@@ -0,0 +1,7 @@
+package aop;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MainConfigOfAOP {
+}
---
+++ src/test/java/cn/bruce/mylib/retest.java
@@ -0,0 +1,13 @@
+package cn.bruce.mylib;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+public class retest {
+    @Test
+    public static void main(String[] args) {
+        Random random = new Random(6);
+        System.out.println(random.toString());
+    }
+}
