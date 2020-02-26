35f9835e20b3c98cd546f0137bf4dc6afd6e979c
whitkoal <46294890+whitkoal@users.noreply.github.com>
2020-02-26 19:37:15 +0800

    2.26

--- src/main/java/aop/MainConfigOfAOP.java
+++ src/main/java/aop/MainConfigOfAOP.java
@@ -1,7 +1,23 @@
 package aop;
 
+import aop.T.LogAspects;
+import aop.T.MathCalculator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @Configuration
 public class MainConfigOfAOP {
+
+    @Bean
+    public MathCalculator calculator(){
+        return new MathCalculator();
+    }
+
+    @Bean
+    public LogAspects logAspects(){
+        return new LogAspects();
+    }
+
 }
--- src/main/java/aop/T/LogAspects.java
+++ src/main/java/aop/T/LogAspects.java
@@ -1,21 +1,35 @@
 package aop.T;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+
+import java.util.Arrays;
+
+@Aspect
 public class LogAspects {
 
-    @befo
-    public  void logStart(){
-        System.out.println("日志打印开始...");
+    @Pointcut("execution(public int aop.T.MathCalculator.*(..))")
+    public void pointCut() {
     }
 
-    public void logEnd(){
-        System.out.println("日志打印结束...");
+    @Before("pointCut()")
+    public void logStart(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        System.out.println(joinPoint.getSignature().getName()+"除法开始...@Befor...参数表： "+ Arrays.asList(args));
     }
 
-    public void logReturn(){
+    @After("pointCut()")
+    public void logEnd(JoinPoint joinPoint) {
+        System.out.println(joinPoint.getArgs()+"日志打印结束...");
+    }
+
+    @AfterReturning("pointCut()")
+    public void logReturn() {
         System.out.println("除法正常返回...");
     }
 
-    public void logException(){
+    @AfterThrowing("pointCut()")
+    public void logException() {
         System.out.println("除法异常...异常信息： {}");
     }
 }
