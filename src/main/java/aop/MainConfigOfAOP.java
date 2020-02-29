46132436d0370e2de2b8012f49eab089f1e2a970
whitkoal <46294890+whitkoal@users.noreply.github.com>
2020-02-29 21:11:06 +0800

    2.29

--- src/main/java/aop/MainConfigOfAOP.java
+++ src/main/java/aop/MainConfigOfAOP.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy
-
 @Configuration
 public class MainConfigOfAOP {
 
