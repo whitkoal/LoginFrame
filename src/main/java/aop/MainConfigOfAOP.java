1a7dbcf818e837bff2a9d64793c51709b7ab8b40
whitkoal <46294890+whitkoal@users.noreply.github.com>
2020-03-01 15:09:39 +0800

    3.1

--- src/main/java/aop/MainConfigOfAOP.java
+++ src/main/java/aop/MainConfigOfAOP.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 public class MainConfigOfAOP {
 
     @Bean
-
     public MathCalculator calculator(){
         return new MathCalculator();
     }
