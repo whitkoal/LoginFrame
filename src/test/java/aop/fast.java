2b308955a85b51e1f0d7169cf2991a4d71348c65
whitkoal <46294890+whitkoal@users.noreply.github.com>
2020-03-11 09:28:15 +0800

    3.11

--- src/test/java/aop/fast.java
+++ src/test/java/aop/fast.java
@@ -19,6 +19,7 @@ public class fast {
         for (int i = 1; i < arr.length; i++) {
             for (int j = i; j > 0 && arr[j] < arr[j - 1]; j--) {
                 swap(arr, j - 1, j);
+
             }
         }
     }
